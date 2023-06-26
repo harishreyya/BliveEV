f80e95c78a8358fb17ec0c16352ff18012043764
Harish Reyya <87423101+harishreyya@users.noreply.github.com>
2023-06-27 02:08:22 +0530

    added AWS deployed server to frontend

--- Backend/src/utils/constants.js
+++ Backend/src/utils/constants.js
@@ -1,5 +1,5 @@
 export const DB_CONNECT = "mongodb://localhost:27017/todo"
-
+// export const DB_CONNECT = "http://13.233.72.120:4000" /*---AWS deployed server-----*/
 export const JWT_TOKEN_SECRET_KEY = "jaimahismationtheway" 
 export const statusCode = {
     SUCCESS:200,
